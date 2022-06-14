a8afbcf776491415838a4cd1a16118ae44255caf
krfcm2 <adf7bn@gmail.com>
2022-06-14 18:57:13 +0200

    v.1

---
+++ pages/numberGenerator/script.js
@@ -0,0 +1,14 @@
+function calculate() {
+    min = document.getElementById("min").value;
+    max = document.getElementById("max").value;
+    if (min == "" || max == "") {
+        alert("Entrez un nombre");
+    }
+    else if (min > max) {
+        alert("Le minimum doit être inférieur au maximum");
+    }
+    else {
+        document.getElementById("result").innerHTML = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+}
